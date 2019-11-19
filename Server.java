05650f015ba2bcc3e2313f2f36b0289628d0193e
Vilarj <lamaravillasan@outlook.com>
2019-11-19 12:00:30 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,52 +1,40 @@
-package socket_mini_chat;
 
-import java.io.DataInputStream;
+package sockets;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Observable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class Server extends Observable implements Runnable {
-    private int port;
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket server = null;
 
-    public Server(int port){
-        this.port = port;
-    }
+        try {
+            server = new ServerSocket(5000);
+            server.setReuseAddress(true);
 
-    @Override
-    public void run() {
-        ServerSocket server; // server object
-        Socket socket = null; // socket object
-        DataInputStream input; // input
+            // Main is implemented to accept many connections (many clients)
+            while (true) {
+                Socket client = server.accept();
+                System.out.println("User IP: " + client.getInetAddress().getHostAddress());
+                Handler clientSock = new Handler(client);
 
-        try{
-            server = new ServerSocket(port);
-            System.out.println("Server initialized");
-
-            while (true){
-                socket = server.accept();
-
-                // Creating the bridge of communication
-                System.out.println("Client is online");
-                input = new DataInputStream(socket.getInputStream());
-
-                String serverMessage = input.readUTF(); // This waits for the client to send something
-                System.out.println(serverMessage);
-
-                // Notify the changes: this idea of Observable
-                this.setChanged(); // it notifies that something is going to change
-                this.notifyObservers(serverMessage); // this is what is being sent
-                this.clearChanged(); // clears the message
-
-
-                socket.close(); // closing the client NOT the server
-                System.out.println("Client is offline");
+                // The background thread will handle each client separately
+                new Thread(clientSock).start();
             }
         }
-        catch (IOException e){
-            Logger.getLogger(socket.Server.class.getName()).log(Level.SEVERE, null, e);
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (server != null) {
+                try {
+                    server.close();
+                }
+                catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 }
