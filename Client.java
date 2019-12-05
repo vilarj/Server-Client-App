20f9550156d7ccc4ec65eec4dbe9f420c24cc2c0
Vilarj <lamaravillasan@outlook.com>
2019-12-05 11:46:14 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,35 +2,42 @@
 package sockets;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
     private static final int PORT = 5000;
-    private static final String HOST = "10.220.90.77";
+    private static final String HOST = "192.168.0.6";
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket(HOST, PORT)) {
-            Scanner scanner = new Scanner(System.in);
-            String line = null;
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // this stores what the user inputs
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // this takes what the server says and output it
+        ServerSocket server = null;
+        OutputStream output = null;
 
-            System.out.print("Enter the name of the file you are looking for: ");
-            while (!"exit".equalsIgnoreCase(line)) {
-                line = scanner.nextLine();
+        try {
+            server = new ServerSocket(5000);
+            server.setReuseAddress(true);
 
-                if ((line.equals("quit") || line.equals("Quit")) || (line.equals("exit") || line.equals("Exit"))) {
-                    System.out.print("~Thank you for using our server~");
-                    System.exit(0);
-                }
+            System.out.print("~~Server initialized~~\n");
 
-                out.println(line);
-                //out.flush();
-                System.out.print("First Client says: " + in.readLine());
+            // Main is implemented to accept many connections (many clients)
+            while (true) {
+                Socket client = server.accept();
+                System.out.println("User IP: {" + client.getInetAddress().getHostAddress() + "}");
+                Handler clientSock = new Handler(client);
+
+                // The background thread will handle each client separately
+                new Thread(clientSock).start();
             }
-            scanner.close(); // Closing the Scanner object
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
-        catch (IOException ex) {ex.printStackTrace();}
     }
 }
